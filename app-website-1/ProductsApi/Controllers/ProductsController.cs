b8a9e32600b0209a1fe461fea97741fd90ef204a
Khie Biggs <khiebigzy@gmail.com>
2019-01-23 17:31:21 +0000

    finish kata

---
+++ app-website-1/ProductsApi/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ProductsApi.Model;
+using ProductsApi.Store;
+
+namespace ProductsApi.Controllers
+{
+    [Route("api/[controller]/{name}")]
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        private ProductStore Store;
+
+        public ProductsController(ProductStore store)
+        {
+            Store = store;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get(string name)
+        {
+            if (name == null)
+            {
+                return Store.GetAll();
+            }
+            return Store.GetAll().Where(p => p.Name == name);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Product value)
+        {
+            Store.Add(value);
+        }
+    }
+}
---
+++ app-website-1/ProductsApi/Store/ProductStore.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using ProductsApi.Model;
+
+namespace ProductsApi.Store
+{
+    public class ProductStore
+    {
+        private List<Product> Products { get; set; }
+        public ProductStore()
+        {
+            Products = new List<Product>();
+        }
+        public IEnumerable<Product> GetAll()
+        {
+            return Products;
+        }
+
+        public void Add(Product product)
+        {
+            Products.Add(product);
+        }
+    }
+}
